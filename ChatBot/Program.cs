764db527bbb06b3cf48022a20db498a4703dead3
Mmabatho76 <ST10460268@rcconnect.edu.za>
2026-04-05 01:56:43 +0200

    Add project files.

---
+++ ChatBot/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatBot
+{
+    internal class Program
+    {
+        class chatBot
+        {
+            public string name;
+
+            public void userInput()
+            {
+               
+            }
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
