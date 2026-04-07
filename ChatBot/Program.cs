f29f35bbb92e113cd3c9764518c2d33393234fc8
Mmabatho76 <ST10460268@rcconnect.edu.za>
2026-04-07 03:56:57 +0200

    Coding

--- ChatBot/Program.cs
+++ ChatBot/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ChatBot
@@ -10,72 +12,111 @@ namespace ChatBot
     {
         class chatBot
         {
-            public string name;
-            public string response;
+            public string name { get; set; }
+            public string response { get; set; }
+            public string BotName { get; set; }
+
+
+            public chatBot()
+            {
+                BotName = "AzeeBot";
+            }
+
 
             public void userInput()
             {
                 Console.Write("Enter your name: ");
                 name = Console.ReadLine();
 
-                Console.ReadLine();
-            }
-        }
-
-        class questions : chatBot
-        {
-            public void display()
-            {
-   
-                Console.WriteLine("Hi " + name + " ,Welcome to Cybersecurity , I hope i will be able to assist you where needed");
-
-                while(string.IsNullOrEmpty(name))
+                while (string.IsNullOrEmpty(name))
                 {
-                    Console.Write("Enter your name: ");
+                    Console.WriteLine("Please enter a valid name: ");
                     name = Console.ReadLine();
+                }
+            }
 
-                    Console.ReadLine();
+            public void MessageInput(string message)
+            {
+                foreach (char m in message)
+                {
+                    Console.WriteLine(m);
+                    Thread.Sleep(20);
+                }
+                Console.WriteLine();
+            }
 
+            public void TypingEffect()
+            {
+                Console.WriteLine(BotName + "is typing");
+                for (int i = 0; i < 3; i++)
+                {
+                   Thread.Sleep(300);
+                    Console.Write(".");
                 }
 
-                Console.WriteLine("\n==========CHATBOT MENU==========\n");
-                Console.WriteLine("1.what is your purpose? ");
-                Console.WriteLine("2.passwords");
-                Console.WriteLine("3.phishing");
-                Console.WriteLine("4.exit");
+            }
 
-                while (!string.IsNullOrEmpty(name))
+            class questions : chatBot
+            {
+                public void displayMenu()
                 {
-                    Console.Write("\n" + name + ": ");
-                    response = Console.ReadLine().ToLower();
 
-                    if(response == "1")
+                    Console.WriteLine("\nHi " + name + " ,Welcome to Cybersecurity , I hope i will be able to assist you where needed\n");
+
+                    bool chatting = true;
+
+                    while (chatting)
                     {
-                        Console.WriteLine(" I am just here to help you to be safe online");
-                    }else if(response == "2")
+                        Console.WriteLine("\n==========CHATBOT MENU==========\n");
+                        Console.WriteLine("1.What is your purpose? ");
+                        Console.WriteLine("2.Pssword Safety Tips");
+                        Console.WriteLine("3.Phishing Awareness");
+                        Console.WriteLine("4.Safe Browsing Tips");
+                        Console.WriteLine("5.Exit");
+                        Console.WriteLine("===================================\n");
+
+                        Console.Write(name + ":");
+                        response = Console.ReadLine().ToLower();
+
+                        TypingEffect();
+
+                    if(response.Contains("1")|| response.Contains("purpose"))
                     {
-                        Console.WriteLine("Passwords should be unique");
-                    }else if (response == "3")
+                            Console.WriteLine(BotName + ": I am just here to help you to be safe online.");
+   
+                    }
+                    else if (response.Contains("2") || response.Contains("password"))
                     {
-                        Console.WriteLine("Phishing is a type of cyberattack where attackers impersonate legitimate entities to deceive individuals into revealing sensitive information, such as passwords and credit card numbers.");
-                    }else if(response == "4")
+                            Console.WriteLine(BotName + ": Passwords should be unique");
+                            Console.WriteLine(BotName + ": Include letters, numbers and symbols.");
+                    }
+                    else if (response.Contains("3") || response.Contains("phishing"))
                     {
-                        break;
+                        Console.WriteLine(BotName + ": Phishing is a type of cyberattack where attackers impersonate legitimate entities to deceive individuals into revealing sensitive information, such as passwords and credit card numbers.");
+                    }
+                    else if (response.Contains("4") || response.Contains("safe browsing") || response.Contains("browse"))
+                    {
+                        Console.WriteLine(BotName + ": Only use secure websites(HTTPS).");
+                    }
+                    else if(response.Contains("5") || response.Contains("exit") || response.Contains("bye"))
+                    {
+                            Console.WriteLine(BotName + ": Goodbye " + name + "Stay Safe Online");
+                            chatting = false;
                     }
                     else
-                    {
-                        Console.WriteLine("I do not quite understand. Please ask questions regarding cybersecurity");
-                    }
+                        {
+                            Console.WriteLine(BotName + ":I do not quite understand. Please ask questions regarding cybersecurity");
+                        }
 
+                    }
                 }
             }
-        }
-        static void Main(string[] args)
-        {
-            questions bot = new questions();
+            static void Main(string[] args)
+            {
+                questions bot = new questions();
 
-            Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.WriteLine(@"
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(@"
 ╔══════════════════════════════════════════════════════╗
 ║              CYBERSECURITY SYSTEM                    ║
 ╠══════════════════════════════════════════════════════╣
@@ -91,12 +132,11 @@ namespace ChatBot
 ╚══════════════════════════════════════════════════════╝
 ");
 
-            Console.ResetColor();
+                Console.ResetColor();
 
-            Console.WriteLine("Hi , Welcome to CyberSecurity Quizz...\n");
-
-            bot.userInput();
-            bot.display();
+                bot.userInput();
+                bot.displayMenu();
+            }
         }
     }
 }
